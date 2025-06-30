d5350005151ed28bc0a81232dd9cc3953960258f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-30 14:49:46 +0200

    added links to navigation

--- js/index.js
+++ js/index.js
@@ -2,10 +2,20 @@ const burgerMenuBtn = document.querySelector("#burger-menu-btn")
 const burgerMenuCloseBtn = document.querySelector("#burger-menu-close-btn")
 const burgerMenu = document.querySelector(".nav__burger-menu")
 const burgerMenuOverlay = document.querySelector(".nav__burger-menu-overlay")
+const burgerMenuLinksContainer = document.querySelector(".nav__links-list--mobile")
+const burgerMenuLinks = burgerMenuLinksContainer.querySelectorAll(".nav__links-list-item")
 
 function closeBurgerMenu() {
     burgerMenu.classList.remove("open")
     burgerMenuOverlay.classList.remove("open")
+}
+
+function closeBurgerMenuWithLink() {
+    burgerMenuLinks.forEach(item => {
+        item.addEventListener("click", () => {
+            closeBurgerMenu()
+        })
+    })
 }
 
 burgerMenuBtn.addEventListener("click", () => {
@@ -20,3 +30,5 @@ burgerMenuCloseBtn.addEventListener("click", () => {
 burgerMenuOverlay.addEventListener("click", () => {
     closeBurgerMenu()
 })
+
+closeBurgerMenuWithLink()
