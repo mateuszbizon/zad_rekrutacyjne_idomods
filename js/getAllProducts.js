04e965fa1c4abdd7141d55a121b6a812bfb2c02a
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-07-01 14:57:38 +0200

    added refetching products with infinite scroll

--- js/getAllProducts.js
+++ js/getAllProducts.js
@@ -1,82 +1,115 @@
 export function fetchAllProducts() {
-    const productsContainer = document.querySelector(".products__items")
-    const productModal = document.querySelector(".product-modal")
-    const productModalIdText = document.querySelector("#product-modal-id-text")
-    const productModalClose = document.querySelector(".product-modal__close")
-    const productModalImg = document.querySelector(".product-modal__img")
+	const productsContainer = document.querySelector(".products__items");
+	const productModal = document.querySelector(".product-modal");
+	const productModalIdText = document.querySelector("#product-modal-id-text");
+	const productModalClose = document.querySelector(".product-modal__close");
+	const productModalImg = document.querySelector(".product-modal__img");
 
-    const pageSizeBoxBtn = document.querySelector(".page-size-box__btn")
-    const pageSizeBoxNumberValue = document.querySelector(".page-size-box__number")
-    const pageSizeBoxList = document.querySelector(".page-size-box__list")
+	const pageSizeBoxBtn = document.querySelector(".page-size-box__btn");
+	const pageSizeBoxNumberValue = document.querySelector(".page-size-box__number");
+	const pageSizeBoxList = document.querySelector(".page-size-box__list");
 
-    let defaultProductsSizePerPage = 14
-    const apiUrl = "https://brandstestowy.smallhost.pl/api/random"
+	const loadMoreProductsContainer = document.querySelector("#load-more-products");
 
-    async function fetchProducts(pageSize = defaultProductsSizePerPage) {
-        const response = await fetch(`${apiUrl}?pageSize=${pageSize}`)
-        const productsData = await response.json()
+	let pageSize = 14;
+	let pageNumber = 1;
+	let isFetching = false;
+	const apiUrl = "https://brandstestowy.smallhost.pl/api/random";
 
-        fillProducts(productsData.data)
-    }
+	async function fetchProducts(pageSize, pageNumberProp) {
+		isFetching = true;
 
-    function fillProducts(products) {
-        products.forEach(product => {
-            const productIdTextValue = product.id < 10 ? `ID: 0${product.id}` : `ID: ${product.id}`
+		const response = await fetch(
+			`${apiUrl}?pageSize=${pageSize}&pageNumber=${pageNumberProp}`
+		);
+		const productsData = await response.json();
 
-            const productItem = document.createElement("div")
-            productItem.classList.add("products__single-item")
-            productItem.setAttribute("onclick", `openModal("${productIdTextValue}", "${product.image}")`)
+		fillProducts(productsData.data);
 
-            const productImg = document.createElement("img")
-            productImg.classList.add("products__single-item-img")
-            productImg.setAttribute("src", `${product.image}`)
+		isFetching = false;
+	}
 
-            const productIdText = document.createElement("span")
-            productIdText.classList.add("body-text")
-            productIdText.classList.add("products__info")
-            productIdText.textContent = productIdTextValue
+	function fillProducts(products) {
+		products.forEach(product => {
+			const productIdTextValue = product.id < 10 ? `ID: 0${product.id}` : `ID: ${product.id}`;
 
-            productItem.appendChild(productIdText)
-            productItem.appendChild(productImg)
-            productsContainer.appendChild(productItem)
-        })
-    }
+			const productItem = document.createElement("div");
+			productItem.classList.add("products__single-item");
+			productItem.setAttribute("onclick", `openModal("${productIdTextValue}", "${product.image}")`);
 
-    window.openModal = function(idText, image) {
-        productModal.showModal()
-        productModalIdText.textContent = idText
-        productModalImg.src = `${image}`
-    }
+			const productImg = document.createElement("img");
+			productImg.classList.add("products__single-item-img");
+			productImg.setAttribute("src", `${product.image}`);
 
-    function generatePageSizeBoxValues() {
-        const pageSizes = [defaultProductsSizePerPage, 24, 36]
+			const productIdText = document.createElement("span");
+			productIdText.classList.add("body-text");
+			productIdText.classList.add("products__info");
+			productIdText.textContent = productIdTextValue;
 
-        pageSizeBoxNumberValue.textContent = defaultProductsSizePerPage
+			productItem.appendChild(productIdText);
+			productItem.appendChild(productImg);
+			productsContainer.appendChild(productItem);
+		});
+	}
 
-        pageSizes.forEach(item => {
-            const pageSizeBoxListItem = document.createElement("li")
+	window.openModal = function (idText, image) {
+		productModal.showModal();
+		productModalIdText.textContent = idText;
+		productModalImg.src = `${image}`;
+	};
 
-            pageSizeBoxListItem.classList.add("page-size-box__list-item")
-            pageSizeBoxListItem.textContent = item
-            pageSizeBoxListItem.addEventListener("click", (e) => {
-                defaultProductsSizePerPage = e.target.textContent
-                pageSizeBoxNumberValue.textContent = e.target.textContent
-                pageSizeBoxList.classList.remove("open")
-            })
+	function generatePageSizeBoxValues() {
+		const pageSizes = [pageSize, 24, 36];
 
-            pageSizeBoxList.appendChild(pageSizeBoxListItem)
-        })
+		pageSizeBoxNumberValue.textContent = pageSize;
 
-    }
+		pageSizes.forEach(item => {
+			const pageSizeBoxListItem = document.createElement("li");
 
-    productModalClose.addEventListener("click", () => {
-        productModal.close()
-    })
+			pageSizeBoxListItem.classList.add("page-size-box__list-item");
+			pageSizeBoxListItem.textContent = item;
+			pageSizeBoxListItem.addEventListener("click", (e) => {
+				pageSize = e.target.textContent;
+				pageSizeBoxNumberValue.textContent = e.target.textContent;
+				pageSizeBoxList.classList.remove("open");
+			});
 
-    pageSizeBoxBtn.addEventListener("click", () => {
-        pageSizeBoxList.classList.toggle("open")
-    })
+			pageSizeBoxList.appendChild(pageSizeBoxListItem);
+		});
+	}
 
-    generatePageSizeBoxValues()
-    fetchProducts()
+	function isScrolledIntoView(el) {
+		const rect = el.getBoundingClientRect();
+		const elemTop = rect.top;
+		const elemBottom = rect.bottom;
+
+		const isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+		return isVisible;
+	}
+
+	function checkToLoadMoreProducts() {
+		const isLoadMoreVisible = isScrolledIntoView(loadMoreProductsContainer);
+
+		if (isLoadMoreVisible && !isFetching) {
+			pageNumber++;
+
+			fetchProducts(pageSize, pageNumber);
+		}
+	}
+
+	productModalClose.addEventListener("click", () => {
+		productModal.close();
+	});
+
+	pageSizeBoxBtn.addEventListener("click", () => {
+		pageSizeBoxList.classList.toggle("open");
+	});
+
+	window.addEventListener("scroll", () => {
+		checkToLoadMoreProducts();
+	});
+
+	generatePageSizeBoxValues();
+	fetchProducts(pageSize, pageNumber);
 }
